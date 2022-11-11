ea8ddbb3acc506ae1703ca62860a6200497779e6
80Quattro <muzyczek2@gmail.com>
2022-11-11 11:34:58 +0100

    Fix tests - LonelyIntegerTest and SparseArraysTest

--- Tests/LonelyIntegerTest.php
+++ Tests/LonelyIntegerTest.php
@@ -5,19 +5,19 @@ final class LonelyIntegerTest extends TestCase
 {
     public function testCase01() {
         $expected = 1;
-        $result = breakingRecords([1]);
+        $result = lonelyInteger([1]);
         $this->assertEquals($expected, $result);
     }
 
     public function testCase02() {
         $expected = 2;
-        $result = breakingRecords([1, 1, 2]);
+        $result = lonelyInteger([1, 1, 2]);
         $this->assertEquals($expected, $result);
     }
 
     public function testCase03() {
         $expected = 2;
-        $result = breakingRecords([0, 0, 1, 2, 1]);
+        $result = lonelyInteger([0, 0, 1, 2, 1]);
         $this->assertEquals($expected, $result);
     }
 }
--- Tests/SparseArraysTest.php
+++ Tests/SparseArraysTest.php
@@ -5,19 +5,19 @@ final class SparseArraysTest extends TestCase
 {
     public function testCase01() {
         $expected = [2, 1, 0];
-        $result = breakingRecords(["aba", "baba", "aba", "xzxb"], ["aba", "xzxb", "ab"]);
+        $result = matchingStrings(["aba", "baba", "aba", "xzxb"], ["aba", "xzxb", "ab"]);
         $this->assertEquals($expected, $result);
     }
 
     public function testCase02() {
         $expected = [1, 0, 1];
-        $result = breakingRecords(["def", "de", "fgh"], ["de", "lmn", "fgh"]);
+        $result = matchingStrings(["def", "de", "fgh"], ["de", "lmn", "fgh"]);
         $this->assertEquals($expected, $result);
     }
 
     public function testCase03() {
         $expected = [1, 3, 4, 3, 2];
-        $result = breakingRecords(
+        $result = matchingStrings(
             ["abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"], 
             ["abcde", "sdaklfj", "asdjf", "na", "basdn"]
         );
